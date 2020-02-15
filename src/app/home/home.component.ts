6a6e63c8dff73baa4223d9acf8a8f4909b927bab
Pritam Bhalerao <pritam.bhalerao@steinnlabs.com>
2020-02-15 18:47:07 +0530

    chatting app integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as socket_io from 'socket.io-client';
+
 import { UserService } from '../shared/services/user.service';
 import { Subscription } from 'rxjs';
 import { AppComponent } from '../app.component';
@@ -13,15 +15,44 @@ import { MainService } from '../shared/services/main.service';
 export class HomeComponent implements OnInit, OnDestroy {
   me: any;
   MyInfoSubscription: Subscription;
+  users: any;
+  // sockets
+  socketEndPoint: 'http://localhost:9697';
+  socket: any;
+  socketId: any;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private userService: UserService,
     private mainService: MainService
   ) {
+    this.users = [{
+      id: 1,
+      name: 'Pritam',
+      email: 'pritam@gmail.com'
+    },
+    {
+      id: 2,
+      name: 'Aditya',
+      email: 'adity@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'Batya',
+      email: 'batya@gmail.com'
+    },
+    {
+      id: 4,
+      name: 'Ambar',
+      email: 'ambar@gmail.com'
+    },
+    ];
+
   }
 
   ngOnInit() {
+    this.socket = socket_io('http://localhost:9697');
+
     const token = window.localStorage.getItem('token');
     if (!token) {
       this.router.navigate(['/login']);
@@ -38,6 +69,25 @@ export class HomeComponent implements OnInit, OnDestroy {
 
       });
     }
+
+    // socket test
+
+    this.socketId = this.socket.socketId;
+    this.socket.on('new_message', (message) => {
+      // this.messages.push(message);
+      console.log('received msg', message);
+    });
+    this.socket.on('connected', async (data) => {
+      this.socketId = data.clientId;
+      console.log('issuing connection', data);
+    });
+  }
+  sendMessage = () => {
+    console.log('calling socket for test')
+    this.socket.emit('send_message', 'Trying to communicate');
+  }
+  onUserSelect(id) {
+    console.log(id);
   }
   ngOnDestroy() {
     if (this.MyInfoSubscription) {
